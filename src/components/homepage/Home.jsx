a4ea3938446d76310b665a4ed60af95d944a6e46
Nurullah Türkoğlu <nurullahtrkglu@gmail.com>
2022-08-13 13:13:12 +0300

    navigator changed

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
   const [messages, setMessages] = useState([]);
   const scrollRef = useRef();
   const socket = useRef();
+  let navigator = useNavigate();
 
   useEffect(() => {
     socket.current = io("ws://localhost:4001");
@@ -106,7 +107,6 @@ const Home = () => {
     navigator("/")
   }
 
-  let navigator = useNavigate();
 
   const searchFriend = (value) => console.log(value);
 
